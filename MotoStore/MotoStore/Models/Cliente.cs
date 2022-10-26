42f5fa828da995cd7d1c43cb8b6fc17371bb9b16
moisesph <mois.mp8@gmail.com>
2022-10-25 21:27:19 -0500

    Empezar#

---
+++ MotoStore/MotoStore/Models/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MotoStore.Models
+{
+    public class Cliente
+    {
+        public string IdCliente { get => IdCliente; set => IdCliente = value; }
+        public string NombreCliente { get => NombreCliente; set => NombreCliente = value; }
+        public string ApellidoCliente { get => ApellidoCliente; set => ApellidoCliente = value; }
+        public string Correo { get => Correo; set => Correo = value; }
+        public int Telefono { get => Telefono; set => Telefono = value; }
+        public string Direccion { get => Direccion; set => Direccion = value; }
+        public string Ciudad { get => Ciudad; set => Ciudad = value; }
+    }
+}
---
+++ MotoStore/MotoStore/Models/CompraMoto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MotoStore.Models
+{
+    public class Moto
+    {
+        public int IdCompra { get => IdCompra; set => IdCompra = value; }
+        public DateTime FechaCompra { get => FechaCompra; set => FechaCompra = value; }
+        public string Marca { get => Marca; set => Marca = value; }
+        public string Modelo { get => Modelo; set => Modelo = value; }
+        public int Cilindraje { get => Cilindraje; set => Cilindraje = value; }
+        public string Freno { get => Freno; set => Freno = value; }
+        public string Llantas { get => Llantas; set => Llantas = value; }
+        public string Refirigeracion { get => Refirigeracion; set => Refirigeracion = value; }
+        public string Suspension { get => Suspension; set => Suspension = value; }
+        public Double ValorMoto { get => ValorMoto; set => ValorMoto = value; }
+        public Double ValorSoat { get => ValorSoat; set => ValorSoat = value; }
+        public Double ValorTotal { get => ValorTotal; set => ValorTotal = value; }
+        public string IdCliente { get => IdCliente; set => IdCliente = value; }
+    }
+}
